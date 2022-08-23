d8ae6a7925114bb10bb4c821fdef3e962f2a87c5
matiesadrian241 <matiesadrian241@gmail.com>
2022-08-23 12:56:23 +0300

    update documentation

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { PageWrapper } from "./Layout.style";
 
 /**
- *  High order component used to render both Characters and Character Profile page and have the same background image
+ *  Higher order component used to render both Characters and Character Profile page and have the same background image
  */
 const withLayout =
   (Component: React.ComponentType) => (props: JSX.IntrinsicAttributes) =>
